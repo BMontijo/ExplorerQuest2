4ed1c2c28f5dd88d07954557b20eb05498393458
BMontijo <juansonos@gmail.com>
2019-03-07 08:00:31 -0500

    Add music controls, Add Character Selection Activity (not set up)

--- app/src/main/java/com/montjo/johnny/explorerquest/MainActivity.java
+++ app/src/main/java/com/montjo/johnny/explorerquest/MainActivity.java
@@ -7,6 +7,9 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    // TODO: open media player
+    //private MediaPlayer mMediaPlayer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,21 +17,36 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // TODO: add sound resource
-        //MediaPlayer mediaPlayer = MediaPlayer.create(R.raw.);
-        //mediaPlayer.start();
+        //mMediaPlayer= MediaPlayer.create(R.raw.);
+        //mMediaPlayer.start();
 
         // Find button and set onClickListener
         Button btnStart = findViewById(R.id.button_start);
         btnStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // TODO: launch Story Activity
                 // Intent to start Story Activity
-                //Intent characterSeletionIntent = new Intent(MainActivity.this, // name of character select.class);
+                Intent characterSelectionIntent = new Intent(MainActivity.this, CharacterSelectionActivity.class);
+                startActivity(characterSelectionIntent);
 
-                //mediaPlayer.release();
-                //mediaPlayer = null;
+                // TODO: uncomment media controls
+                //releasePlayer();
             }
         });
+    }
+
+    private void releasePlayer() {
+        //if (mMediaPlayer != null) {
+            //mMediaPlayer.release();
+            //mMediaPlayer = null;
+        //}
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        // release media player
+        //releasePlayer();
     }
 }
